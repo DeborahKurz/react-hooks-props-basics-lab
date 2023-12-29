9d065a8bb84f91cdf60e301e6df898f0519f7dd4
Deborah Kurz <DeborahKurz6@gmail.com>
2023-12-29 10:46:27 -0700

    Complete Lab

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,17 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
 // pass this data down as props to the child component(s) that need it!
-import User from "../data/user";
-console.log(User);
+import user from "../data/user";
+// console.log(User);
 
+console.log(user.links.github)
 function App() {
   return (
     <div>
       <NavBar />
-      <Home name="Lisa" city="Denver"/>
-      <About bio="I made this!" linkedin="https://linkedin.com/in/liza" github="https://githbub.com/liza"/>
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio="I made this!" github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,7 +1,13 @@
 import React from "react";
  
-function Links(){
-    
-}
+function Links(props){
+    // console.log("Links: ", props)
+    return (
+        <div>
+            <h3>Links</h3>
+            <a href={props.github}>{props.github}</a>
+            <a href={props.linkedin}>{props.linkedin}</a>
+        </div>
+)}
  
 export default Links;
